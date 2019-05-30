acb02df31317ddd1874fdef8041106b38e3a2e41
呆狗 <daigou@myaidoctor.com>
2019-05-30 10:46:29 +0800

    lalla

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Screen />
+  </div>
+</template>
+
+<script>
+import Screen from './components/Screen.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Screen
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+html, body{
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Screen.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="screen" class="screen">
+    <div class="item">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+    <div class="item">4</div>
+  </div>
+</template>
+
+<script>
+import initScreen from '../utils/initScreen'
+export default {
+  name: 'Screen',
+  mounted () {
+    initScreen(1920, 1080, 'screen')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.screen{
+  background-color: #a9a9a9;
+}
+.item{
+  float: left;
+  width: 960px;
+  height: 540px;
+  background-color: green;
+  font-size: 200px;
+  color: white;
+}
+.item:nth-of-type(1){
+  background-color: gold;
+}
+.item:nth-of-type(2){
+  background-color: red;
+}
+.item:nth-of-type(3){
+  background-color: blue;
+}
+</style>
